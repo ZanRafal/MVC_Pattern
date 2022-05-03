02660bce6f72c33be2515153d953526339fb9767
ZanRafal <01159225@pw.edu.pl>
2022-05-03 10:53:46 +0200

    Dodano DataModel.java

---
+++ src/model/DataModel.java
@@ -0,0 +1,17 @@
+package model;
+
+import bean.User;
+
+import java.util.List;
+
+public class DataModel {
+    List<User> userList;
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+}
